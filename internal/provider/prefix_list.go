7ffedce62314acc4926c5a2d9aa28839928ac992
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-02-25 08:18:44 -0800

    fixing data source params

--- internal/provider/prefix_list.go
+++ internal/provider/prefix_list.go
@@ -224,7 +224,7 @@ func prefixListSchema(isResource bool, rmKeys []string) map[string] *schema.Sche
     }
 
     if !isResource {
-        computed(ans, "", []string{RulestackName, "name"})
+        computed(ans, "", []string{ConfigTypeName, RulestackName, "name"})
     }
 
     return ans
