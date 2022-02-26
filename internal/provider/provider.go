d985d23d4f6bf9c3eb3f63605c8137f17c994040
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-02-26 14:31:17 -0800

    configuring the document builder

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 	"fmt"
+    "strings"
 
 	"github.com/paloaltonetworks/cloud-ngfw-aws-go"
 
@@ -12,19 +13,21 @@ import (
 )
 
 func init() {
-	// Set descriptions to support markdown syntax, this will be used in document generation
-	// and the language server.
 	schema.DescriptionKind = schema.StringMarkdown
 
-	// Customize the content of descriptions when output. For example you can add defaults on
-	// to the exported descriptions if present.
-	// schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
-	// 	desc := s.Description
-	// 	if s.Default != nil {
-	// 		desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
-	// 	}
-	// 	return strings.TrimSpace(desc)
-	// }
+    schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
+        desc := s.Description
+
+        if s.Default != nil {
+            desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
+        }
+
+        if s.Deprecated != "" {
+            desc += " " + s.Deprecated
+        }
+
+        return strings.TrimSpace(desc)
+    }
 }
 
 func New(version string) func() *schema.Provider {
