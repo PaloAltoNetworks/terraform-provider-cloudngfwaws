7a4fd8cdab10a4865800c7d908265d0e9360edf7
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-03-11 15:41:12 -0800

    being more explicit with the provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    cloudngfwaws = {
+      source  = "paloaltonetworks/terraform-provider-cloudngfwaws"
+      version = "1.0.0"
+    }
+  }
+}
+
 provider "cloudngfwaws" {
+  region           = "eu-west-1"
   json_config_file = "~/.cloudngfwaws_creds.json"
 }
