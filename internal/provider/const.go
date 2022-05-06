424da69ee5799acc6ba1a09e97354cb9189c1eea
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-05-06 00:52:23 -0700

    updating constants

--- internal/provider/const.go
+++ internal/provider/const.go
@@ -5,21 +5,15 @@ const IdSeparator = ":"
 
 // Various param name constants that show up in multiple resources / data sources.
 const (
-	RulestackName       = "rulestack"
-	GlobalRulestackName = "globalrulestack"
-	RuleListName        = "rule_list"
-	ConfigTypeName      = "config_type"
-	TagsName            = "tags"
+	RulestackName  = "rulestack"
+	RuleListName   = "rule_list"
+	ConfigTypeName = "config_type"
+	TagsName       = "tags"
+	ScopeName      = "scope"
 )
 
 // Valid values for ConfigTypeName within data sources.
 const (
 	CandidateConfig = "candidate"
 	RunningConfig   = "running"
-)
-
-// Valid values for EndpointMode when creating firewalls
-const (
-	CustomerManaged = "CustomerManaged"
-	ServiceManaged  = "ServiceManaged"
 )
