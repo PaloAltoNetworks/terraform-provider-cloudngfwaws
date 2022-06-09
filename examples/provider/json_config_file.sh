5155d607c228bf68f95fa3570a32c09f68bc6075
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-06-08 17:24:48 -0700

    updating json config in example documentation

--- examples/provider/json_config_file.sh
+++ examples/provider/json_config_file.sh
@@ -1,5 +1,5 @@
 {
-  "host": "api.us-east-1.aws.cloudngfw.com",
+  "host": "api.us-east-1.aws.cloudngfw.paloaltonetworks.com",
   "region": "us-east-1",
   "arn": "arn:aws:iam::123456789:role/MyRole"
 }
